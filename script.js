7af7aa3c6621c41e87e1dd7ff7b1e1012b9994fe
DwebAgency2 <webdev2agency@gmail.com>
2025-04-10 23:33:53 +0100

    new file:   assets/favicon.png 	new file:   assets/logo light logo.png 	new file:   assets/portrait_1.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ resumePortfolioTabBtns.forEach((resumePortfolioTabBtns, i)=>{
    Service modal open/close function
 ===================================================== */
 
+/*
 const serviceCardWithModals = document.querySelectorAll(".service-container .card-with-modal");
 
 serviceCardWithModals.forEach((serviceCardWithModal) => {
@@ -93,6 +94,41 @@ serviceCardWithModals.forEach((serviceCardWithModal) => {
    })
 })
 
+*/
+
+const serviceCardWithModals = document.querySelectorAll(".service-container .card-with-modal");
+
+serviceCardWithModals.forEach((serviceCardWithModal) => {
+   const serviceCard = serviceCardWithModal.querySelector(".service-card");
+   const serviceBackDrop = serviceCardWithModal.querySelector(".service-modal-backdrop");
+   const modalCloseBtn = serviceCardWithModal.querySelector(".modal-close-btn");
+   const serviceModal = serviceCardWithModal.querySelector(".service-modal");
+
+   serviceCard.addEventListener("click", () => {
+      // Open the modal
+      serviceBackDrop.style.display = "flex";
+
+      setTimeout(() => {
+         serviceBackDrop.classList.add("active");
+      }, 100);
+
+      setTimeout(() => {
+         serviceModal.classList.add("active");
+      }, 300);
+   });
+
+   modalCloseBtn.addEventListener("click", () => {
+      // Close the modal
+      setTimeout(() => {
+         serviceBackDrop.style.display = "none";
+      }, 500);
+
+      setTimeout(() => {
+         serviceBackDrop.classList.remove("active");
+         serviceModal.classList.remove("active");
+      }, 100);
+   });
+});
 
 
 
